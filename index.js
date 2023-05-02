f90dd4f6fb64312d5c33995882440908a3a04910
José Ferreira <joseguedes.dev@gmail.com>
2023-05-02 02:19:29 -0300

    Refactor the project code
    Adding the function to display the events and renaming the page files

--- index.js
+++ index.js
@@ -2,20 +2,19 @@ require('dotenv').config();
 const express = require('express');
 const cors = require('cors');
 
-
 const app = express();
 app.use(express.json());
+app.use(express.urlencoded({extended:false}));
 app.use(cors());
 const port = process.env.API_PORT;
+const pontoController = require('./controllers/PontoController')
+
+pontoController.sincronizar();
+
+app.get('/', pontoController.sincronizar);
+app.get('/pontos', pontoController.getPontos);
+app.post('/pontos', pontoController.addPonto);
 
 app.listen(port, ()=>{
     console.log(`App running on port ${port}.`);
 });
-
-const pontoController = require('./controllers/PontoController');
-
-app.post('/pontos', pontoController.addPonto);
-
-app.get('/pontos', pontoController.getPontos);
-
-app.get('/', pontoController.sincronizar);
